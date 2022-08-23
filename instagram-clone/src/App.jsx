e57a7ca11c361c372ba8fb5011e932df19de7209
Vaisakh <vaisakhpu007@gmail.com>
2022-08-24 00:02:20 +0530

    basic folder structure and scss setup

---
+++ instagram-clone/src/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./app.module.scss";
+
+function App() {
+  return (
+    <div>
+      <h2 className={styles.heading}>Hello world</h2>
+    </div>
+  );
+}
+
+export default App;
